398b9ba7deb75990af116ed8912a64b6c9fe45fb
Olivier21 <olivier.lafleur21@gmail.com>
2022-01-12 07:29:04 -0500

    little update

--- Enemies/Bandits.cs
+++ Enemies/Bandits.cs
@@ -1,17 +1,21 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Bandits : KinematicBody2D
 {
 	private enum state{
 		IDLE,
+		WANDER,
 		CHASE
 	};
 	
 	private int ACCELERATION = 300;
 	private int MAX_SPEED = 50;
 	private int FRICTION = 200;
-	
+
+	private int wander_target_range = 4;
+
 	private state currentState;
 	
 	public Vector2 Velocity;
@@ -19,13 +23,6 @@ public class Bandits : KinematicBody2D
 	Area2D playerDetectionZone;
 	KinematicBody2D player;
 	AnimatedSprite aSprite;
-	
-	public override void _Ready()
-	{
-		Velocity = Vector2.Zero;
-		playerDetectionZone = GetNode<Area2D>("PlayerDetectionZone");
-		aSprite = GetNode<AnimatedSprite>("AnimatedSprite");		
-	}
 	
 	
 }
