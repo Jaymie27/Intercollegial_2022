a7300dca10d0e070589e5cdb1d511698824e63d3
Jaymie27 <j_deschenes27@hotmail.com>
2022-01-13 11:08:34 -0500

    changes to bandit

--- Level1/Forest/Forest.cs
+++ Level1/Forest/Forest.cs
@@ -16,8 +16,21 @@ public class Forest : Node2D
 			GetTree().ChangeScene("res://Level1/Mine/Mine.tscn");
 		}
 	}
-
+	
+	private void _on_ToVillage_area_entered(Area2D area)
+	{
+	  	if(area.IsInGroup("player"))
+		{
+			GetTree().ChangeScene("res://Level1/Village/Village2.tscn");
+		}
+	}
+	private void _on_ToShipping_area_entered(Area2D area)
+	{
+		if(area.IsInGroup("player"))
+		{
+			GetTree().ChangeScene("res://Level1/Delivery/Delivery.tscn");
+		}
+	}
 }
-
 
 
