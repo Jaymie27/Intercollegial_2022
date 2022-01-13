8fd7eccf86a5ce07c6f1d497f6a91708de428e12
CheapTR <vieux.torchon@outlook.com>
2022-01-13 14:02:02 -0500

    ending screen good

--- EndingScene.cs
+++ EndingScene.cs
@@ -6,6 +6,7 @@ public class EndingScene : Node2D
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
+	bool released = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -26,8 +27,21 @@ public class EndingScene : Node2D
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public override void _Process(float delta)
+	{
+		if(Input.IsActionPressed("ui_attack"))
+			{
+				if(released == true)
+				{
+					MC.life = 10;
+					MC.ressources = 0;
+					MC.rocks = 0;
+					GetTree().ChangeScene("res://TitleScreen/TitleScreen.tscn");
+				}
+			}
+			else
+			{
+				released = true;
+			}
+	}
 }
